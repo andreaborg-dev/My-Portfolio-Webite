7577877b4f0d8531f10231e6ace9feb226c3c0ca
Andrea <work.andreaborg@gmail.com>
2025-09-22 20:20:40 +0200

    Navbar V.1, New Files and Folders

---
+++ Javascript/index.js
@@ -0,0 +1,12 @@
+function SidebarMenuPhone() {
+    var icon = document.getElementById("HamburgerMenu")
+    var center = document.getElementById("center")
+    var navbar = document.getElementById("mainnav")
+    icon.addEventListener("click", function() {
+        center.classList.toggle("horizontalcenter")
+        navbar.classList.toggle("navOFF")
+        icon.classList.toggle("HamburgerMenuON")
+        icon.classList.toggle("HamburgerMenuOFF")
+    })
+}
+SidebarMenuPhone()
