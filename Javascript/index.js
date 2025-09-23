a524c54278e3718330327de37bdc1d0409ed3584
Andrea <work.andreaborg@gmail.com>
2025-09-23 11:37:49 +0200

    nav V2, New Background

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,11 +2,14 @@ function SidebarMenuPhone() {
     var icon = document.getElementById("HamburgerMenu")
     var center = document.getElementById("center")
     var navbar = document.getElementById("mainnav")
-    icon.addEventListener ("touchstart", function() {
+        function showhidemenu() {
         center.classList.toggle("horizontalcenter")
         navbar.classList.toggle("navOFF")
         icon.classList.toggle("HamburgerMenuON")
         icon.classList.toggle("HamburgerMenuOFF")
-    })
+}
+
+icon.addEventListener("click", showhidemenu);
+icon.addEventListener("touchend", showhidemenu);
 }
 SidebarMenuPhone()
