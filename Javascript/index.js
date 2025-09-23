3c4987d017c7f88fd37bc3458e21482213374c97
Andrea <work.andreaborg@gmail.com>
2025-09-23 14:51:11 +0200

    added close button on navbar

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,15 +1,16 @@
 function SidebarMenuPhone() {
     var icon = document.getElementById("HamburgerMenu")
+    var close_icon = document.getElementById("close")
     var center = document.getElementById("center")
     var navbar = document.getElementById("mainnav")
         function showhidemenu() {
-        center.classList.toggle("horizontalcenter")
         navbar.classList.toggle("navOFF")
-        icon.classList.toggle("HamburgerMenuON")
-        icon.classList.toggle("HamburgerMenuOFF")
 }
+
 
 icon.addEventListener("click", showhidemenu);
 icon.addEventListener("touchend", showhidemenu);
+close_icon.addEventListener("click", showhidemenu);
+close_icon.addEventListener("touchend", showhidemenu);
 }
 SidebarMenuPhone()
